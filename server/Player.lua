68c2865491040a3c54e102925e41647c97c82e0a
Neadshots <67221160+Neadshots@users.noreply.github.com>
2020-09-17 02:55:22 +0300

    Update Player.lua

--- server/Player.lua
+++ server/Player.lua
@@ -3,7 +3,9 @@ Player = Element('Player') -- inherit the Element class
 -- overriding the Element class __call function
 getmetatable(Player).__call = function(_, name) return setmetatable({element = getPlayerFromName(name)}, {__index = Player}) end
 
-Player.getName = function(element) return getPlayerName(element.element) end
-Player.getIP   = function(element) return getPlayerIP(element.element) end
+Player.getName   = function(element) return getPlayerName(element.element) end
+Player.getIP     = function(element) return getPlayerIP(element.element) end
 Player.getPing   = function(element) return getPlayerPing(element.element) end
-Player.getMoney   = function(element) return getPlayerMoney(element.element) end
+Player.getMoney  = function(element) return getPlayerMoney(element.element) end
+Player.getMoney  = function(element) return getPlayerMoney(element.element) end
+Player.kick      = function(element,responsiblePlayer,reason) return getPlayerMoney(element.element,responsiblePlayer,reason) end
