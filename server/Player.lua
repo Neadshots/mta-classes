c3eceb242b543036abcf9eff663aa9fdc62af73b
Mustafa Resul KEL <mkel3686@hotmail.com>
2020-09-16 23:20:37 +0300

    Added Player Class for server

---
+++ server/Player.lua
@@ -0,0 +1,10 @@
+Player = Element('Player') -- inherit the Element class
+
+-- overriding the Element class __call function
+getmetatable(Player).__call = function(_, name) return setmetatable({element = getPlayerFromName(name)}, {__index = Player}) end
+
+Player.getName = function(element) return getPlayerName(element.element) end
+Player.getIP   = function(element) return getPlayerIP(element.element) end
+
+-- defining localPlayer
+localPlayer = Player(getPlayerName(localPlayer)) -- xd
