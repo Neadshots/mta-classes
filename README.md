101e801f15b19e5a7d785ddcb6fa9448b5351639
Mustafa Resul KEL <mkel3686@hotmail.com>
2020-09-16 23:16:41 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mta-classes
-Its just overrided MTA Classes for high performance
+It's just overrided MTA Classes for high performance
+
+
+> with getPosition function
+
+| Traditional | OOP (MTA) | OOP (With this repo) |
+| :---------: | :-------: | :------------------: |
+| 287ms       | 2720ms    | 461ms                |
--- test/main.lua
+++ test/main.lua
@@ -1,9 +1,26 @@
 -- example
 local wicky = Player('wicky')
 
+-- traditional
+-- local start = getTickCount()
+-- for i=1, 1000000 do
+--    getElementPosition(wicky)
+-- end
+-- print('the test results (Player | Traditional): ' .. getTickCount() - start)
+-- 287ms
+
+-- oop (mta)
+-- local start = getTickCount()
+-- for i=1, 1000000 do
+--    wicky:getPosition()
+-- end
+-- print('the test results (Player | OOP (MTA)): ' .. getTickCount() - start)
+-- 2720ms
+
+-- oop (with this repo)
 local start = getTickCount()
 for i=1, 1000000 do
-   localPlayer:getPosition()
+   wicky:getPosition()
 end
-print('the test results (Player): ' .. getTickCount() - start)
--- probably higher than 400ms but its lower to the mta oop class
+print('the test results (Player | OOP): ' .. getTickCount() - start)
+-- 461ms
