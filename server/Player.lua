bfa0b2142451e68566c318c89b55121fee6fb1f9
Mustafa Resul KEL <52138983+wicky-r@users.noreply.github.com>
2020-09-17 02:47:26 +0300

    Removed localPlayer definiton

--- server/Player.lua
+++ server/Player.lua
@@ -7,6 +7,3 @@ Player.getName = function(element) return getPlayerName(element.element) end
 Player.getIP   = function(element) return getPlayerIP(element.element) end
 Player.getPing   = function(element) return getPlayerPing(element.element) end
 Player.getMoney   = function(element) return getPlayerMoney(element.element) end
-
--- defining localPlayer
-localPlayer = Player(getPlayerName(localPlayer)) -- xd
